daffe7f5b5f3ffbd7d73b3678d840e5ce28e1671
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2024-08-06 17:08:52 +0300

    minimal changes to build on el9 via LCG_104

--- setup_cvmfs.sh
+++ setup_cvmfs.sh
@@ -1,13 +1,23 @@
 export CURRENTDIR=$(pwd)
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/Geant4-env.sh
-cd /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/geant4make.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/ROOT/6.18.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/HepMC/2.06.10/x86_64-centos7-gcc8-opt/HepMC-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/jsoncpp/1.8.4/x86_64-centos7-gcc8-opt/jsoncpp-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/CMake/3.14.3/x86_64-centos7-gcc8-opt/CMake-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/cmaketools/1.8/x86_64-centos7-gcc8-opt/cmaketools-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/301/x86_64-centos7-gcc8-opt/pythia8env-genser.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/fastjet/3.3.2/x86_64-centos7-gcc8-opt/fastjet-env.sh
-export PYTHIA8DATA=$PYTHIA8_HOME/share/Pythia8/xmldoc   
+echo "Setting up GEANT4"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/Geant4/11.1.2/x86_64-el9-gcc11-opt/Geant4-env.sh
+cd /cvmfs/sft.cern.ch/lcg/releases/LCG_104/Geant4/11.1.2/x86_64-el9-gcc11-opt/share/Geant4/geant4make/
+echo "Setting up Geant4make"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/Geant4/11.1.2/x86_64-el9-gcc11-opt/share/Geant4/geant4make/geant4make.sh
+echo "Setting up ROOT"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/ROOT/6.28.04/x86_64-el9-gcc11-opt/ROOT-env.sh
+echo "Setting up HepMC"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/HepMC/2.06.11/x86_64-el9-gcc11-opt/HepMC-env.sh
+echo "Setting up jsoncpp"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/jsoncpp/1.9.3/x86_64-el9-gcc11-opt/jsoncpp-env.sh
+echo "Setting up CMake"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/CMake/3.26.2/x86_64-el9-gcc11-opt/CMake-env.sh
+echo "Setting up cmaketools"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/cmaketools/1.8/x86_64-el9-gcc11-opt/cmaketools-env.sh
+echo "Setting up Pythia8"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/MCGenerators/pythia8/309/x86_64-el9-gcc11-opt/pythia8env-genser.sh
+export PYTHIA8_HOME=/cvmfs/sft.cern.ch/lcg/releases/LCG_104/MCGenerators/pythia8/309/x86_64-el9-gcc11-opt
+export PYTHIA8DATA=$PYTHIA8_HOME/share/Pythia8/xmldoc
+echo "Setting up FastJet"
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_104/fastjet/3.4.1/x86_64-el9-gcc11-opt/fastjet-env.sh
 cd $CURRENTDIR
